787ee7e9ef0ceacea1aac0b904875420ecfc7e7e
Cristian Velasco Caceres <cristian@gradiweb.com>
2022-05-07 00:11:11 -0500

    test server without

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import Router from "koa-router";
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
-//const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 
 Shopify.Context.initialize({
