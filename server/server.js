9d84fe6ca51f2d71f65c544101e5c54524d6f081
Cristian Velasco Caceres <cristian@gradiweb.com>
2022-05-08 00:40:48 -0500

    test server without

--- server/server.js
+++ server/server.js
@@ -6,11 +6,16 @@ import Shopify, { ApiVersion } from "@shopify/shopify-api";
 import Koa from "koa";
 import next from "next";
 import Router from "koa-router";
+import {installExecute} from './init/install';
+import {getTokenAccesAPI} from './init/tokenFresh';
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({
+  dev,
+});
+const handle = app.getRequestHandler();
 
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
@@ -18,7 +23,7 @@ Shopify.Context.initialize({
   SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\/|\/$/g, ""),
   API_VERSION: ApiVersion.October20,
-  IS_EMBEDDED_APP: true,
+  IS_EMBEDDED_APP: false,
   // This should be replaced with your preferred storage strategy
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
 });
@@ -35,10 +40,28 @@ app.prepare().then(async () => {
     createShopifyAuth({
       async afterAuth(ctx) {
         // Access token and shop available in ctx.state.shopify
-        const { shop, scope } = ctx.state.shopify;
+        const { shop, accessToken, scope } = ctx.state.shopify;
         const host = ctx.query.host;
         ACTIVE_SHOPIFY_SHOPS[shop] = scope;
 
+        const response = await Shopify.Webhooks.Registry.register({
+          shop,
+          accessToken,
+          path: "/webhooks",
+          topic: "APP_UNINSTALLED",
+          webhookHandler: async (topic, shop, body) =>
+            delete ACTIVE_SHOPIFY_SHOPS[shop],
+        });
+
+        if (!response.success) {
+          console.log(
+            `Failed to register APP_UNINSTALLED webhook: ${response.result}`
+          );
+        }
+
+        installExecute(shop);
+
+        
         // Redirect to app with shop parameter upon auth
         ctx.redirect(`/?shop=${shop}&host=${host}`);
       },
@@ -46,11 +69,48 @@ app.prepare().then(async () => {
   );
 
   const handleRequest = async (ctx) => {
-    await app.getRequestHandler(ctx.req, ctx.res);
+    await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
   };
 
+  router.post("/webhooks", async (ctx) => {
+    try {
+      await Shopify.Webhooks.Registry.process(ctx.req, ctx.res);
+      console.log("ahora pasa aqui.....");
+      console.log(`Webhook processed, returned status code 200`);
+    } catch (error) {
+      console.log(`Failed to process webhook: ${error}`);
+    }
+  });
+
+  router.post(
+    "/graphql",
+    verifyRequest({ returnHeader: true }),
+    async (ctx, next) => {
+      await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);
+    }
+  );
+
+
+/* test request front ------------------------------*/
+  router.get("/tokenapi", async (ctx) => {
+    
+    const token = await getTokenAccesAPI();
+   
+    ctx.res.statusCode = 200;
+    ctx.body = token;
+
+  })
+
+  router.get("/geturl", async (ctx) => {
+     ctx.body = process.env.APP_BACKEND_URL;
+  })
+/* test request front --------------------------*/
+
+
+  router.get("(/_next/static/.*)", handleRequest); // Static content is clear
+  router.get("/_next/webpack-hmr", handleRequest); // Webpack content is clear
   router.get("(.*)", async (ctx) => {
     const shop = ctx.query.shop;
 
@@ -60,8 +120,10 @@ app.prepare().then(async () => {
       ctx.redirect(`/auth?shop=${shop}`);
     } else {
       await handleRequest(ctx);
+      
     }
   });
+
 
   server.use(router.allowedMethods());
   server.use(router.routes());
